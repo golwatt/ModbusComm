f155d3fadeb54d663f1b6039423eb02288c0d6a8
golwatt <golwatt@163.com>
2012-05-16 23:24:47 +0800

    Add ModbusComm project

---
+++ ModbusComm/ModbusComm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModbusComm
+{
+    class Test
+    {
+        public void method(object sender, EventArgs e)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+
+    class Program
+    {
+        static public event EventHandler pe;
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            pe += t.method;
+            Console.ReadKey();
+        }
+    }
+} 
